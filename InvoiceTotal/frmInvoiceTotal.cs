6a8d50caa007c8652a015821fbf1beccd86a75b2
tepper-d <104649969+tepper-d@users.noreply.github.com>
2022-05-06 01:07:55 -0700

    Add project files.

---
+++ InvoiceTotal/frmInvoiceTotal.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* ******************************************************
+* CIS 123: Introduction to Object-Oriented Programming  *
+* Murach C# 7th ed, Chapter 6: How to code methods      *
+*                and event handlers                     *
+* Exercise 6-3 Enhance the Invoice Total Application    *
+*       Base code and form design provided by Murach    *
+*       Exercise Instructions: pg. 192                  *
+* Dominique Tepper, 05MAY2022                           *
+* ******************************************************/
+
+namespace InvoiceTotal
+{
+    public partial class frmInvoiceTotal : Form
+    {
+        public frmInvoiceTotal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            string customerType = txtCustomerType.Text;
+            decimal subtotal = Convert.ToDecimal(txtSubtotal.Text);
+            decimal discountPercent = .0m;
+
+            if (customerType == "R")
+            {
+                if (subtotal < 100)
+                    discountPercent = .0m;
+                else if (subtotal >= 100 && subtotal < 250)
+                    discountPercent = .1m;
+                else if (subtotal >= 250)
+                    discountPercent = .25m;
+            }
+            else if (customerType == "C")
+            {
+                if (subtotal < 250)
+                    discountPercent = .2m;
+                else
+                    discountPercent = .3m;
+            }
+            else
+            {
+                discountPercent = .4m;
+            }
+
+            decimal discountAmount = subtotal * discountPercent;
+            decimal invoiceTotal = subtotal - discountAmount;
+
+            txtDiscountPercent.Text = discountPercent.ToString("p1");
+            txtDiscountAmount.Text = discountAmount.ToString("c");
+            txtTotal.Text = invoiceTotal.ToString("c");
+
+            txtCustomerType.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
